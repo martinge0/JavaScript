5a7a2c69ee51beee5a2297c5a5debef8af6b6834
martinge0 <martingeo@lycos.com>
2019-09-12 14:08:27 +0300

    Final version

---
+++ Return Largest Numbers in Arrays.js
@@ -0,0 +1,10 @@
+function largestOfFour(arr) {
+    let maxValues = [];
+for (let i=0; i<4; i++){
+      maxValues.push(Math.max(...arr[i]))
+    }
+    let indexOfMaxValue = maxValues.indexOf(Math.max(...maxValues));
+    return arr[indexOfMaxValue];
+  }
+
+  console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
