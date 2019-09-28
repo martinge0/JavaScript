dd7776005152177750f1944d84ce602478d62bcb
martinge0 <martingeo@lycos.com>
2019-09-28 09:44:49 +0300

    Falsy Bouncer

---
+++ Falsy Bouncer.js
@@ -0,0 +1,9 @@
+function bouncer(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i]){
+            result.push(arr[i]);
+        }
+    }
+      return result;
+    }
