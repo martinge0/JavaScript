2e9653e2430fa982d76d888569b793456d489fd8
martinge0 <martingeo@lycos.com>
2019-10-01 22:36:05 +0300

    with for loop  and  break  at lable

--- Mutations.js
+++ Mutations.js
@@ -1,35 +1,54 @@
-let str = ["floor" , "for"];
-let charZero = [];
-let charOne = [];
+function mutation(str) {
 
-for (let i = 0; i< str[0].length; i++){
-        charZero.push(str[0].toLowerCase().charAt(i));
-}
+    let charZero = [];
+    let charOne = [];
+    
+    for (let i = 0; i< str[0].length; i++){
+            charZero.push(str[0].toLowerCase().charAt(i));
+    }
+    
+    for (let i = 0;i < str[1].length; i++ ){
+        charOne.push(str[1].toLowerCase().charAt(i));
+    }
+    let charZeroSorted = charZero.sort();
+    let charOneSorted = charOne.sort();
+    
+    let lengthShort = 0;
+    let lengthLong = 0;
+    let charShort = [];
+    let charLong = [];
+    
+    if(charZeroSorted.length <= charOneSorted.length){
+        lengthShort = charZeroSorted.length;
+        lengthLong = charOneSorted.length;
+        charShort = charZeroSorted;
+        charLong = charOneSorted;
+    }
+    else{
+        lengthShort = charOneSorted.length;
+        lengthLong = charZeroSorted.length;
+        charShort = charOneSorted;
+        charLong = charZeroSorted;
+    }
+    
+    let result = true;
+    check:
+    for (let i = 0; i < lengthShort; i++){
+        for(let j = 0; j < lengthLong; j++){
+            if(charShort[i] == charLong[j]){
+                result = true;
+                break;
+            }
+            else{
+                result = false;
+                if(j == lengthLong-1){
+                    return result;
+                    break check;
+                }            
+            }
+        }
+    }
+      return result;
+    }
 
-for (let i = 0;i < str[1].length; i++ ){
-    charOne.push(str[1].toLowerCase().charAt(i));
-}
-let charZeroSorted = charZero.sort();
-let charOneSorted = charOne.sort();
-let strZeroSorted = charZeroSorted.join();
-let strOneSorted = charOneSorted.join();
-
-let regex = "";
-let testString = "";
-if(strZeroSorted.length <= strOneSorted.length){
-    regex = new RegExp(strZeroSorted);
-    testString = strOneSorted;
-}
-else{
-    regex = new RegExp(strOneSorted);
-    testString = strZeroSorted;
-}
-let result = true;
-
-result = regex.test(testString);
-
-console.log(strZeroSorted);
-console.log(strOneSorted);
-console.log("-------------------------------");
-console.log(regex)
 
