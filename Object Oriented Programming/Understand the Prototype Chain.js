5c36f3019b7746064a5bb1ed619247b883eb372c
martinge0 <martingeo@lycos.com>
2019-10-05 13:01:39 +0300

    Understand the Prototype Chain

---
+++ Object Oriented Programming/Understand the Prototype Chain.js
@@ -0,0 +1,11 @@
+function Dog(name) {
+    this.name = name;
+  }
+  
+  let beagle = new Dog("Snoopy");
+  
+  Dog.prototype.isPrototypeOf(beagle);  // => true
+  
+  
+  Object.prototype.isPrototypeOf(Dog.prototype);
+  
