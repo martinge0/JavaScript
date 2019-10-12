49950eb363f36e4ad54a6b590be468bb70a06560
martinge0 <martingeo@lycos.com>
2019-10-12 12:04:32 +0300

    Implement map on a Prototype

---
+++ Functional Programming/Implement map on a Prototype.js
@@ -0,0 +1,15 @@
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback){
+  var newArray = [];
+
+  this.forEach(a => newArray.push(callback(a)));
+
+  return newArray;
+
+};
+
+var new_s = s.myMap(function(item){
+  return item * 2;
+});
