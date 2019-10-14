5a76743a31d6022e4dd4d948af861a923608cf25
martinge0 <martingeo@lycos.com>
2019-10-14 12:06:29 +0300

    Split a String into  Array Using the split Method

---
+++ Functional Programming/Split a String into an Array Using the split Method.js
@@ -0,0 +1,7 @@
+function splitify(str) {
+    // Add your code below this line
+  return str.split(/[^a-z^0-99]/i);
+    
+    // Add your code above this line
+  }
+  splitify("Hello World,I-am code");
